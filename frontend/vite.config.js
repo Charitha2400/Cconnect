4fbab9f4ded4ccaf757080bce80108118d9659cc
Your Name <you@example.com>
2026-04-28 11:51:54 +0530

    git deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/FSD_Project/',
   plugins: [react()],
   server: {
     host: true,                 // 👈 REQUIRED for external access
